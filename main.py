1c70ab4fdd1464af5e8c385657d38fdddb5bf925
Pratima Kumari <pratimakumari2305@gmail.com>
2023-09-12 11:22:27 +0200

    init

--- main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-data = np.load("modelnet10.npz", allow_pickle=True)
-train_voxel = data["train_voxel"] # Training 3D voxel samples
-test_voxel = data["test_voxel"] # Test 3D voxel samples
-train_labels = data["train_labels"] # Training labels (integers from 0 to 9)
-test_labels = data["test_labels"] # Test labels (integers from 0 to 9)
-class_map = data["class_map"] # Dictionary mapping the labels to their class names.
